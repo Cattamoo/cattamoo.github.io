a3f77d0678a328c9896ae7e7cf2193ea048eb145
Cattamoo <ouob77@kakao.com>
2023-03-29 16:14:51 +0900

    2022.03.29  Skill => Level 뒤에 색상 수정

--- src/components/Level.tsx
+++ src/components/Level.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Level({ level, color }: Props) {
 	return (
 		<div className="flex items-center w-full">
-			<div className="overflow-hidden w-full h-2 bg-zinc-50">
+			<div className="overflow-hidden w-full h-2" style={{ backgroundColor: `${color}20` }}>
 				<div className="h-full" style={{ backgroundColor: color, width: `${level}%` }} />
 			</div>
 		</div>
