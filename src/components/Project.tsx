58d127f5d2b3188f8ff036b5ef8e0f52056cb3e0
Cattamoo <ouob77@kakao.com>
2023-04-08 02:45:38 +0900

    2022.04.08  프로젝트 모습 수정

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {FaGithub} from "react-icons/fa";
 import {MdOutlineOpenInNew} from "react-icons/md";
+import {RiCalendarTodoFill} from "react-icons/ri";
 
 export type ProjectType = {
 	img?: string;
@@ -13,20 +14,20 @@ export type ProjectType = {
 
 export default function Project({ img = '', title, date, content = '', tags = [], link = {} }: ProjectType) {
 	return (
-		<li className="min-h-full bg-amber-50 p-4 shadow-lg flex flex-col items-center gap-1">
+		<li className="relative min-h-full bg-amber-50 p-4 shadow-lg flex flex-col items-center gap-1">
+			<div className="absolute -left-3 top-3 text-sm flex gap-0.5 items-center py-2 px-6 font-semibold text-zinc-600/75 bg-amber-200 shadow">
+				<RiCalendarTodoFill className="mr-2 text-amber-600" /><span>{date.start}</span>~{date.end && <span>{date.end}</span>}
+			</div>
 			<img className="w-full aspect-square object-left object-cover rounded-lg" src={img} alt={`${title}`} />
-			<div className="w-full flex justify-between items-center">
-				<div className="flex gap-0.5 items-center py-0.5 px-1 text-xs font-semibold text-zinc-600 bg-amber-200 rounded-lg">
-					<span>{date.start}</span>~{date.end && <span>{date.end}</span>}
-				</div>
-				<div className="flex gap-1 items-center text-lg">
+			<div className="w-full relative">
+				<h2 className="text-center text-lg font-bold text-black">
+					{ title }
+				</h2>
+				<div className="absolute top-1/2 right-0 -translate-y-2/4 flex gap-1 items-center text-lg">
 					{ link?.github && <a rel="noreferrer" className="text-black/50 hover:text-black/75" href={link.github} target="_blank"><FaGithub/></a> }
 					{ link?.page && <a rel="noreferrer" className="text-black/50 hover:text-black/75" href={link.page} target="_blank"><MdOutlineOpenInNew/></a> }
 				</div>
 			</div>
-			<h2 className="text-lg font-bold text-black">
-				{ title }
-			</h2>
 			<ul className="flex gap-1.5 text-sm">
 				{
 					tags.map((tag, idx) => <li className="bg-amber-100 px-1 pb-0.5 rounded-lg" key={`tag_${title}_${idx}`}>{`#${tag}`}</li>)
