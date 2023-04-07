cf119ea09f1968b2a9d380fe9a75daec172384d6
Cattamoo <ouob77@kakao.com>
2023-04-07 20:17:28 +0900

    2022.04.07  프로젝트 날짜 추가 & 프로젝트 순서 변경

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,15 +5,19 @@ import {MdOutlineOpenInNew} from "react-icons/md";
 export type ProjectType = {
 	img?: string;
 	title: string;
+	date: { start: string; end?: string; };
 	content?: string;
 	tags?: string[];
 	link?: { github?: string, page?: string };
 }
 
-export default function Project({ img = '', title, content = '', tags = [], link = {} }: ProjectType) {
+export default function Project({ img = '', title, date, content = '', tags = [], link = {} }: ProjectType) {
 	return (
 		<li className="min-h-full bg-amber-50 p-4 shadow-lg flex flex-col items-center gap-1">
 			<img className="w-full aspect-square object-left object-cover rounded-lg" src={img} alt={`${title}`} />
+			<p className="self-end text-xs font-semibold flex gap-0.5 items-center py-0.5 px-1 mt-2 text-zinc-600 bg-amber-200 rounded">
+				<span>{date.start}</span>~{date.end && <span>{date.end}</span>}
+			</p>
 			<h2 className="flex items-center gap-2 text-lg font-bold text-black">
 				{ title }
 				{ link?.github && <a rel="noreferrer" className="text-black/50 hover:text-black/75" href={link.github} target="_blank"><FaGithub/></a> }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -10,33 +10,37 @@ import "swiper/css/effect-cards";
 
 const projects: ProjectType[] = [
 	{
-		title: 'Markdown Editor',
-		img: 'https://user-images.githubusercontent.com/87294942/226334946-b12df126-06dc-4a6b-a210-1335927c0697.png',
+		title: 'YouTube',
+		date: { start: '2022-11', end: '2023-04'},
+		img: 'https://user-images.githubusercontent.com/87294942/230587851-f0c574aa-16e9-400a-baa0-de7fcc030095.png',
 		tags: ['React', 'TailwindCSS'],
-		content: 'Markdown 파일을 저장/수정 할 수 있는 웹어플리케이션입니다.',
-		link: { github: 'https://github.com/Cattamoo/editor', page: 'https://cattamoo.github.io/editor' }
+		content: 'YouTube 클론코딩입니다.',
+		link: { github: 'https://github.com/Cattamoo/youtube', page: 'https://youtube-cattamoo.vercel.app/' }
 	},
 	{
 		title: 'Colorful Log Maker',
+		date: { start: '2023-01', end: '2023-03'},
 		img: 'https://user-images.githubusercontent.com/87294942/226336432-bcbb4c36-15f5-4001-a8ab-759b625380fa.png',
 		tags: ['React', 'TailwindCSS', 'Typescript'],
 		content: 'console.log 를 커스터마이징할 수 있는 웹어플리케이션입니다.',
 		link: { github: 'https://github.com/Cattamoo/colorful-log-maker', page: 'https://cattamoo.github.io/colorful-log-maker' }
 	},
 	{
+		title: 'Markdown Editor',
+		date: { start: '2022-11', end: '2023-01'},
+		img: 'https://user-images.githubusercontent.com/87294942/226334946-b12df126-06dc-4a6b-a210-1335927c0697.png',
+		tags: ['React', 'TailwindCSS'],
+		content: 'Markdown 파일을 저장/수정 할 수 있는 웹어플리케이션입니다.',
+		link: { github: 'https://github.com/Cattamoo/editor', page: 'https://cattamoo.github.io/editor' }
+	},
+	{
 		title: 'Like Momentum',
+		date: { start: '2022-11', end: '2023-01'},
 		img: 'https://user-images.githubusercontent.com/87294942/226338171-3ee6ef0c-0d00-4a34-aa81-804e04b0d4bf.png',
 		tags: ['JQuery'],
 		content: 'Momentum 클론코딩(개인 대시보드 웹어플리케이션)입니다.',
 		link: { github: 'https://github.com/j-dea/like.momentum', page: 'https://j-dea.github.io/like.momentum' }
 	},
-	{
-		title: 'YouTube',
-		img: 'https://user-images.githubusercontent.com/87294942/230587851-f0c574aa-16e9-400a-baa0-de7fcc030095.png',
-		tags: ['React', 'TailwindCSS'],
-		content: 'YouTube 클론코딩입니다.',
-		link: { github: 'https://github.com/Cattamoo/youtube', page: 'https://youtube-cattamoo.vercel.app/' }
-	}
 ]
 
 export default function Projects() {
