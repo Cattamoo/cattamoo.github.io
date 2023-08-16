adf685d4abfbc74010b12092cc3909f95f96125c
Cattamoo <ouob77@kakao.com>
2023-08-16 19:29:51 +0900

    리뉴얼을 위한 데이터 이동

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,55 +1,11 @@
-import PageLayout from "../components/layouts/PageLayout";
-import Title from "../components/commons/Title";
-import Project, {ProjectType} from "../components/Project";
-import {Swiper, SwiperSlide} from "swiper/react";
-import {EffectCards} from "swiper";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-cards";
-
-
-const projects: ProjectType[] = [
-	{
-		title: 'ANYTHING',
-		date: { start: '2023-05-24', end: '2023-05-26'},
-		img: 'https://github.com/Cattamoo/anything/assets/87294942/01b417fd-5271-4c0d-9249-e9e0a49068b9',
-		tags: ['React', 'Typescript', 'TailwindCSS', 'Redux', 'firebase'],
-		content: '구글 로그인을 연동하여 다른 사용자들과 소통할 수 있는 웹 어플리케이션입니다.',
-		link: { github: 'https://github.com/Cattamoo/anything', page: 'https://anything-cattamoo.vercel.app' }
-	},
-	{
-		title: 'MEMO',
-		date: { start: '2023-04-20', end: '2023-05-23'},
-		img: 'https://github.com/Cattamoo/memo/assets/87294942/ca2db2ba-ff3d-4cff-ba52-d6e666c323e4',
-		tags: ['React', 'Typescript', 'TailwindCSS', 'Redux'],
-		content: '개인 메모 웹 어플리케이션입니다.',
-		link: { github: 'https://github.com/Cattamoo/memo', page: 'https://memo-cattamoo.vercel.app' }
-	},
-	{
-		title: 'Weather',
-		date: { start: '2023-05-18', end: '2023-05-21'},
-		img: 'https://github.com/Cattamoo/weather/assets/87294942/3e55cd5f-a7e0-45cf-b119-1c1cf1a212c3',
-		tags: ['Vue', 'Typescript', 'TailwindCSS'],
-		content: '날씨 API를 사용한 날씨정보 웹 어플리케이션 입니다.',
-		link: { github: 'https://github.com/Cattamoo/weather', page: 'https://weather-cattamoo.vercel.app' }
-	},
-	{
-		title: 'Colorful Log Maker',
-		date: { start: '2023-01', end: '2023-03'},
-		img: 'https://user-images.githubusercontent.com/87294942/226336432-bcbb4c36-15f5-4001-a8ab-759b625380fa.png',
-		tags: ['React', 'TailwindCSS', 'Typescript'],
-		content: 'console.log 를 커스터마이징할 수 있는 웹어플리케이션입니다.',
-		link: { github: 'https://github.com/Cattamoo/colorful-log-maker', page: 'https://cattamoo.github.io/colorful-log-maker', gist: 'https://gist.github.com/Cattamoo/dc60bc06d190359a8158b304869bc35b' }
-	},
-	{
-		title: 'Markdown Editor',
-		date: { start: '2022-11', end: '2023-01'},
-		img: 'https://user-images.githubusercontent.com/87294942/226334946-b12df126-06dc-4a6b-a210-1335927c0697.png',
-		tags: ['React', 'TailwindCSS'],
-		content: 'Markdown 파일을 저장/수정 할 수 있는 웹어플리케이션입니다.',
-		link: { github: 'https://github.com/Cattamoo/editor', page: 'https://cattamoo.github.io/editor' }
-	},
-]
+import PageLayout from "../components/layouts/PageLayout";
+import Title from "../components/commons/Title";
+import Project from "../components/Project";
+import { projects } from "../data/projects";
 
 export default function Projects() {
 	return (
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,39 +1,8 @@
 import PageLayout from "../components/layouts/PageLayout";
 import Title from "../components/commons/Title";
 import SubTitle from "../components/commons/SubTitle";
-import Skill, {SkillType} from "../components/Skill";
-import {
-	SiCss3, SiGit, SiGraphql,
-	SiHtml5,
-	SiJavascript, SiNextdotjs, SiNodedotjs,
-	SiOracle,
-	SiPostgresql,
-	SiReact, SiRedux,
-	SiTailwindcss,
-	SiTypescript
-} from "react-icons/si";
-
-const skills: { [key: string]: SkillType[] } = {
-	'Front-end': [
-		{ Icon: SiHtml5, name: 'HTML', level: 63, color: '#ec6231' },
-		{ Icon: SiCss3, name: 'CSS', level: 57.2, color: '#264de4' },
-		{ Icon: SiJavascript, name: 'Javascript', level: 73, color: '#f0db4f' },
-		{ Icon: SiTypescript, name: 'Typescript', level: 47, color: '#007acc' },
-		{ Icon: SiReact, name: 'React', level: 63, color: '#61dbfb' },
-		{ Icon: SiRedux, name: 'Redux', level: 43, color: '#764abc' },
-		{ Icon: SiTailwindcss, name: 'TailwindCSS', level: 53, color: '#38bdf8' },
-		{ Icon: SiNextdotjs, name: 'Next.js', level: 22, color: '#000000' },
-	],
-	'Back-end': [
-		{ Icon: SiOracle, name: 'Oracle', level: 23, color: '#f80000' },
-		{ Icon: SiPostgresql, name: 'Postgresql', level: 43, color: '#336791' },
-		{ Icon: SiNodedotjs, name: 'Node.js', level: 32, color: '#68a063' },
-		{ Icon: SiGraphql, name: 'GraphQL', level: 21, color: '#e535ab' },
-	],
-	'etc': [
-		{ Icon: SiGit, name: 'Git', level: 59, color: '#f1502f' },
-	]
-}
+import Skill from "../components/Skill";
+import { skills } from "../data/skills";
 
 export default function Skills() {
 	return (
