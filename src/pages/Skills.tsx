fb0edaeaf54ffe4205c5c1662cb9fac06cbe55ce
Cattamoo <ouob77@kakao.com>
2023-08-17 00:06:56 +0900

    Skill 수정
      width 수정

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -9,14 +9,13 @@ export default function Skills() {
 		<PageLayout>
 			<Title className="text-orange-300">Skills</Title>
 			<div className="w-80 mt-1.5 mb-3">
-				<p>제가 보유한 스킬입니다.</p>
-				<ul className="mx-2">
+				<ul>
 					{
 						skillDescriptions.map((description) => <SkillDescription key={description.description} {...description} />)
 					}
 				</ul>
 			</div>
-			<ul className="grid grid-cols-4 gap-2 p-2 md:grid-cols-5 lg:grid-cols-6">
+			<ul className="w-80 grid grid-cols-4 gap-2 p-2 md:grid-cols-5 lg:grid-cols-6">
 			{
 				Object.values(skills).flat().sort((a, b) => b.level - a.level).map((skill: SkillType) => <Skill key={skill.name} {...skill} />)
 			}
