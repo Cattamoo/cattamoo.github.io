d2a9e3a6b543d5302f1ba02946006e3bc07ee911
Cattamoo <ouob77@kakao.com>
2023-08-28 22:46:20 +0900

    Skills 수정
      각 스킬 레벨에 대한 필터링 기능 추가

--- src/components/SkillDescription.tsx
+++ src/components/SkillDescription.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {SkillDescriptionType} from "../data/skills";
 
-type Props = SkillDescriptionType;
+type Props = SkillDescriptionType & {
+	isCurrent: boolean;
+	setFilter: (scores: SkillDescriptionType["scores"]) => void;
+	clearFilter: () => void;
+};
 
-export default function SkillDescription({ scores, description }: Props) {
+export default function SkillDescription({ isCurrent, scores, description, setFilter, clearFilter }: Props) {
+	console.log(scores, isCurrent)
 	return (
 		<li className="flex gap-1">
-			<span className="text-center bg-brandDark text-white text-xs self-center px-2 rounded-lg">
+			<span
+				className={`cursor-pointer text-center ${isCurrent ? 'bg-brand text-brandDark' : 'bg-brandDark text-white'} text-xs self-center px-2 rounded-lg`}
+				onClick={!isCurrent ? () => setFilter(scores) : clearFilter}
+			>
 				{
 					scores.min || <span className="text-transparent">00</span>
 				}
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -2,22 +2,45 @@ import PageLayout from "../components/layouts/PageLayout";
 import Title from "../components/commons/Title";
 import Skill from "../components/Skill";
 import SkillDescription from "../components/SkillDescription";
-import {skillDescriptions, skills, SkillType} from "../data/skills";
+import {skillDescriptions, SkillDescriptionType, skills, SkillType} from "../data/skills";
+import {useState} from "react";
 
 export default function Skills() {
+	const [selected, setSelected] = useState<SkillDescriptionType["scores"] | null>(null);
+	const filteringSkills = (skill: SkillType) => {
+		if(selected !== null) {
+			if((skill.level >= (selected.min ? selected.min : 0)) && (skill.level <= (selected.max ? selected.max : 100))) {
+				return true;
+			}
+		} else {
+			return true;
+		}
+	}
+	const handleSetFilter = (scores: SkillDescriptionType["scores"]) => {
+		setSelected(scores);
+	}
+	const handleClearFilter = () => {
+		setSelected(null);
+	}
 	return (
 		<PageLayout>
 			<Title className="text-brandDark">Skills</Title>
 			<div className="w-80 mt-1.5 mb-3">
 				<ul>
 					{
-						skillDescriptions.map((description) => <SkillDescription key={description.description} {...description} />)
+						skillDescriptions.map((description) => <SkillDescription
+							key={description.description}
+							isCurrent={!!selected && selected === description.scores}
+							setFilter={handleSetFilter}
+							clearFilter={handleClearFilter}
+							{...description}
+						/>)
 					}
 				</ul>
 			</div>
 			<ul className="w-80 grid grid-cols-4 gap-2 p-2 lg:grid-cols-5">
 			{
-				Object.values(skills).flat().sort((a, b) => b.level - a.level).map((skill: SkillType) => <Skill key={skill.name} {...skill} />)
+				Object.values(skills).flat().filter(filteringSkills).sort((a, b) => b.level - a.level).map((skill: SkillType) => <Skill key={skill.name} {...skill} />)
 			}
 			</ul>
 		</PageLayout>
